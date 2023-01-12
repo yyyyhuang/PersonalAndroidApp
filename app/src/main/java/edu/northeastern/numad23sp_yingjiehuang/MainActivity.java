d5e2547aa100ae412d3b1342a713063094f40c0d
yyyyhuang <yingjie.huang2013@gmail.com>
2023-01-12 15:25:15 -0800

    Initial commit

---
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.northeastern.numad23sp_yingjiehuang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button_send);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Yingjie Huang, huang.yingj@northeastern.edu", Toast.LENGTH_LONG).show();//display the text of button1
+            }
+        });
+    }
+
+
+}
