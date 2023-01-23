69b48b36558ab9397d94c64f4ee125a91d05787c
yyyyhuang <yingjie.huang2013@gmail.com>
2023-01-23 11:16:23 -0800

    clicky button added

---
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/DetailActivity.java
@@ -0,0 +1,17 @@
+package edu.northeastern.numad23sp_yingjiehuang;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+    private static final String TAG = "DetailActivity___";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/MainActivity.java
@@ -2,6 +2,7 @@ package edu.northeastern.numad23sp_yingjiehuang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +10,36 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button) findViewById(R.id.button_send);
+        button2 = (Button) findViewById(R.id.clicky);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Yingjie Huang, huang.yingj@northeastern.edu", Toast.LENGTH_LONG).show();//display the text of button1
+                Toast.makeText(getApplicationContext(), "Yingjie Huang, huang.yingj@northeastern.edu", Toast.LENGTH_LONG).show();//display the text of button
+                // startActivity(new Intent(MainActivity.this, DetailActivity.class));
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(getApplicationContext(), "Yingjie Huang, huang.yingj@northeastern.edu", Toast.LENGTH_LONG).show();//display the text of button
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+            }
+        });
+
+
     }
+
+
 
 
 }
