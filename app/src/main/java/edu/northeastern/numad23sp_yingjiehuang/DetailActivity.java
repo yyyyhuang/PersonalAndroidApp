4ad501ef567643a204a60198702c51b14a2ecfb2
yyyyhuang <yingjie.huang2013@gmail.com>
2023-01-23 12:04:12 -0800

    Clicky activity added

--- app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/DetailActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/DetailActivity.java
@@ -2,6 +2,8 @@ package edu.northeastern.numad23sp_yingjiehuang;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +15,23 @@ public class DetailActivity extends AppCompatActivity {
         Log.v(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    public void onClick(View view) {
+        TextView change = findViewById(R.id.textView);
+        int theId = view.getId();
+        if (theId == R.id.button_A) {
+            change.setText("Pressed: A");
+        } else if (theId == R.id.button_B) {
+            change.setText("Pressed: B");
+        } else if (theId == R.id.button_C) {
+            change.setText("Pressed: C");
+        } else if (theId == R.id.button_D) {
+            change.setText("Pressed: D");
+        } else if (theId == R.id.button_E) {
+            change.setText("Pressed: E");
+        } else if (theId == R.id.button_F) {
+            change.setText("Pressed: F");
+        }
     }
 }
