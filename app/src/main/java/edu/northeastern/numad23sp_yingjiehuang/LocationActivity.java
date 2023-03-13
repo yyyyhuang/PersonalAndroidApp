d19463c274a765f2e7cfe437c049d80ae7772830
yyyyhuang <yingjie.huang2013@gmail.com>
2023-03-13 12:51:56 -0700

    location activity created

---
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/LocationActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_yingjiehuang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
--- app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_yingjiehuang/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
     private Button button4;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.clicky);
         button3 = (Button) findViewById(R.id.links);
         button4 = (Button) findViewById(R.id.prime);
+        button5 = (Button) findViewById(R.id.location);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LocationActivity.class));
+            }
+        });
     }
 
 
